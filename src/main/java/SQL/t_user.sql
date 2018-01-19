4924191d7db6892e5eaa090750e99322d4bf79a1
jsphLim <1806291589@qq.com>
2018-01-20 01:21:43 +0800

    Update t_user.sql

--- src/main/java/SQL/t_user.sql
+++ src/main/java/SQL/t_user.sql
@@ -1,6 +1,17 @@
-/**
- * Created by ly on 2017/11/30.
- */
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : zhewaida
+Source Server Version : 50540
+Source Host           : localhost:3306
+Source Database       : userinfo
+
+Target Server Type    : MYSQL
+Target Server Version : 50540
+File Encoding         : 65001
+
+Date: 2018-01-20 01:20:32
+*/
 
 SET FOREIGN_KEY_CHECKS=0;
 
@@ -9,15 +20,17 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
-  `id` bigint(11) NOT NULL auto_increment,
-  `user_name` varchar(255) default NULL COMMENT '用户名',
-  `user_phone` varchar(20) default NULL COMMENT '手机号',
-  `user_email` varchar(255) default NULL COMMENT '邮箱地址',
-  `user_pwd` varchar(32) default NULL COMMENT '加盐后用户密码',
-  `pwd_salt` varchar(6) default NULL COMMENT 'MD5盐',
-  `create_time` datetime default NULL COMMENT '创建时间',
-  `modify_time` datetime default NULL COMMENT '最后修改时间',
-  `is_delete` tinyint(4) default NULL COMMENT '是否删除，0-未删除；1-已删除',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='用户登录表';
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(40) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_pwd` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('1', 'linyue', '1050450245@qq.com', '4607E782C4D86FD5364D7E4508BB10D9');
+INSERT INTO `t_user` VALUES ('3', 'yansheng', '1526232@gmail.com', 'E10ADC3949BA59ABBE56E057F20F883E');
+INSERT INTO `t_user` VALUES ('4', 'linyyy', '16161@qq.com', 'E10ADC3949BA59ABBE56E057F20F883E');
+INSERT INTO `t_user` VALUES ('5', 'test1', '123456@gmail.com', 'E10ADC3949BA59ABBE56E057F20F883E');
