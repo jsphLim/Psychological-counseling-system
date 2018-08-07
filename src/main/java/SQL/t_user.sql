e7386dd4a50c928969f661687a575e419c075d01
jsphLim <jsphlim@stu2016.jnu.edu.cn>
2018-08-07 13:39:30 +0800

    Update t_user.sql

--- src/main/java/SQL/t_user.sql
+++ src/main/java/SQL/t_user.sql
@@ -30,7 +30,7 @@ CREATE TABLE `t_user` (
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES ('1', 'linyue', '1050450245@qq.com', '4607E782C4D86FD5364D7E4508BB10D9');
+INSERT INTO `t_user` VALUES ('1', 'linyue', '105055@qq.com', '4607E782C4D86FD5364D7E4508BB10D9');
 INSERT INTO `t_user` VALUES ('3', 'yansheng', '1526232@gmail.com', 'E10ADC3949BA59ABBE56E057F20F883E');
 INSERT INTO `t_user` VALUES ('4', 'linyyy', '16161@qq.com', 'E10ADC3949BA59ABBE56E057F20F883E');
 INSERT INTO `t_user` VALUES ('5', 'test1', '123456@gmail.com', 'E10ADC3949BA59ABBE56E057F20F883E');
